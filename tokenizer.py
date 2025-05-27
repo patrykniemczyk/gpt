b16e59bd2ecf0d04b3b5bdd2ecbb3c40a7638607
Patryk <patryk.niemczyk12345@gmail.com>
2025-05-27 20:24:24 +0200

    finish tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -33,23 +33,11 @@ def merge_pair(ids_list, pair, idx):
 
 class Tokenizer:
 
-    def __init__(self, vocab_size=2048, special_tokens={
-        '<pad>': 256, '<unk>': 257, '<eos>': 258,
-    }):
+    def __init__(self, vocab_size=2048):
 
         self.vocab_size = vocab_size
-        self.special_tokens = special_tokens if special_tokens else {}  # str -> int
         self.merges = {}  # (int, int) -> int
-
         self.vocab = {i: bytes([i]) for i in range(256)}  # int -> bytes
-
-        for special_token, idx in self.special_tokens.items():
-            if idx < 256:
-                raise ValueError(f"Special token index {idx} must be >= 256")
-            if idx in self.vocab:
-                raise ValueError(
-                    f"Special token index {idx} conflicts with existing vocab index")
-            self.vocab[idx] = special_token.encode('utf-8')
 
     def train(self, texts):
         texts_bytes = [text.encode('utf-8') for text in texts]
@@ -90,14 +78,3 @@ class Tokenizer:
     def decode(self, ids_list):
         text_bytes = b"".join(self.vocab[id] for id in ids_list)
         return text_bytes.decode('utf-8', errors='replace')
-
-
-tokenizer = Tokenizer()
-texts = ["Hello, world!", "This is a test.", "GPT is great!",
-         "Let's tokenize this text.", "Special tokens are important."]
-tokenizer.train(texts)
-
-for text in texts:
-    encoded = tokenizer.encode(text)
-    decoded = tokenizer.decode(encoded)
-    print(f"Original: {text}\nEncoded: {encoded}\nDecoded: {decoded}\n")
